b829795b6ee9dd943922843f7ebafe43d751fe1a
imanishx <manishbiswal754@gmail.com>
2024-12-07 16:56:44 +0000

    port change

--- controllers/url.js
+++ controllers/url.js
@@ -16,7 +16,7 @@ async function handleGenerateNewShortURL(req, res) {
   });
 
    return res.status(201).json({
-    shortUrl: `http://trmzip.club:${process.env.PORT}/${shortID}`,   
+    shortUrl: `http://www.trmzip.club:${process.env.PORT}/${shortID}`,   
     shortId: shortID,                            
   });
 }
